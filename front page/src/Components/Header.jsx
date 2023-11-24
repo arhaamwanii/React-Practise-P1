8f94ff2dbeee57f0045dbbdd0657f8772fcf94cc
arhaamwanii <arhamwani765@gmail.com>
2023-11-24 15:49:07 +0530

    .

--- front page/src/Components/Header.jsx
+++ front page/src/Components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
     <>
         <div className='head-container'>
             <div className=' logo'>
-                <img style={{height:100, width: 100}} src={nike} alt="nike img " />    
+                <img style={{height:60, width: 200}} src="https://banner2.cleanpng.com/20180716/xck/kisspng-nike-free-swoosh-logo-lauras-girls-karlsruhe-karlsruhe-5b4d5b3f803c05.9518714915317962875253.jpg" alt="nike img " />    
             </div>
 
             <div className='middle-options '>
---
+++ front page/src/Components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <>
+    <div className='hero-container' >
+        <div className='left'>
+            <h1>There is only one Way Forward thats Straight UP</h1>
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore consequatur a incidunt odio nam blanditiis cum, voluptatem, sunt nostrum id deleniti! Asperiores, impedit nisi? Quidem architecto recusandae iusto modi deleniti!</h3>
+            <div>
+              <button>
+                <img src="" alt="shop now" />
+              </button>
+              <button>
+                <img src="" alt="category" />
+              </button>
+            </div>
+        </div>
+
+        <div className='right'>
+              <img src="https://images.pexels.com/photos/19229575/pexels-photo-19229575/free-photo-of-beach-stretch.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="man standing on the rod " />
+        </div>
+       </div>
+    </>
+  )
+}
+
+export default Hero
